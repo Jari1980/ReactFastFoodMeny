94b6561bb53fddd3d0570ed1ffd575492579b2d9
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-03-21 12:54:10 +0100

    technical component in place, next make this prettier

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useThemeContext } from "./context";
 import menyItemData from "../data/MenyItems"
+import { Button } from "react-bootstrap";
 
 const Order = (props) => {
   const { order, setOrder } = useThemeContext("");
@@ -13,17 +14,35 @@ const Order = (props) => {
     menyFiltered.push(menyItemData[item - 1])
   }
 
-  console.log(menyFiltered)
-  //const Broccoli = menyItemData.filter((item) => item.id === 1);
+  function addFood(id){
+    setOrder([...order, id])
+  }
+
+  function removeFood(id){
+    const index = order.indexOf(id);
+    setOrder([
+        ...order.slice(0, index),
+        ...order.slice(index + 1)
+    ])
+  }
+
+  function total(){
+    let sum = 0;
+    order.forEach(element => {
+        sum = sum + menyItemData[element - 1].price;
+    });
+    return sum;
+  }
   
 
   return (
   <div>
     <ol>
         {menyFiltered.map((item) => (
-            <li key={item.id}>Item: {item.title} Amount:{order.filter(function(value){return value === item.id}).length} Price:{item.price * order.filter(function(value){return value === item.id}).length}</li>
+            <li key={item.id}>{item.title} X:{order.filter(function(value){return value === item.id}).length} <Button onClick={() => addFood(item.id)}>Add</Button> <Button onClick={() => removeFood(item.id)}>Remove</Button> Price:{item.price * order.filter(function(value){return value === item.id}).length}</li>
         ))}
     </ol>
+    <span>Total {total()}</span>
   </div>
   );
 };
